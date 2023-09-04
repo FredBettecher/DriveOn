a9a4e2b104adfb73825738ff51b36060b1f912ca
FredBettecher <fredbettecher@gmail.com>
2023-09-04 15:51:39 -0300

    feat: create get started page

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,38 @@
-const arr = [
+const getStarted = [
   {
-  image: 'images/solucao-1-teste-programador.png',
+  image: 'public/images/solucao-1-teste-programador.png',
   title: 'Create your <br> account',
-  description: "Complete your account with some basic information inlcuding your home your work location and your working hours."
+  description: "Complete your account <br> with some basic information <br> inlcuding your home <br> your work location and <br> your working hours."
   },
   {
-    image: 'images/solucao-2-teste-programador.png',
+    image: 'public/images/solucao-2-teste-programador.png',
     title: 'Plan your <br> journeys',
-    description: "Open the app, click the agenda tab and enter your working hours for the coming week. Then you'll be presented with a list of our drivers."
+    description: "Open the app, <br> click the agenda tab <br> and enter your working hours <br> for the coming week. <br> Then you'll be presented <br> with a list of our drivers."
   },
   {
-    image: 'images/solucao-3-teste-programador.png',
+    image: 'public/images/solucao-3-teste-programador.png',
     title: 'Meet your <br> driver',
-    description: "Use our chat to get to know your met with our driver. You'll be picked up at your regular location, or agree on an alternative pick-up location"
+    description: "Use our chat to get to know <br> your met with our driver. <br> You'll be picked up at <br> your regular location, <br> or agree on an alternative <br> pick-up location."
   },
   {
-    image: 'images/solucao-4-teste-programador.png',
+    image: 'public/images/solucao-4-teste-programador.png',
     title: 'Enjoy your <br> journey',
-    description: "Get in, sit back and enjoy your journey that takes you directly to your destination with some free cup of coffee from us."
+    description: "Get in, sit back <br> and enjoy your journey <br> that takes you directly <br> to your destination <br> with some free <br> cup of coffee from us."
   },
 ];
+
+const getStartedPage = document.getElementById('get-started-cards');
+
+getStarted.forEach(item => {
+  const div = document.createElement('div')
+  const img = document.createElement('img');
+  const title = document.createElement('h3');
+  const description = document.createElement('p');
+
+  img.src = item.image;
+  title.innerHTML = item.title;
+  description.innerHTML = item.description;
+
+  div.append(img, title, description)
+  getStartedPage.append(div);
+});
