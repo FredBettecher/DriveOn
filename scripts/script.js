55cf897fd2972ebdcdbfff8515a3cfe417bd1c81
FredBettecher <fredbettecher@gmail.com>
2023-09-04 14:14:31 -0300

    feat: move images to /public/images folder

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,21 @@ const arr = [
   {
   image: 'images/solucao-1-teste-programador.png',
   title: 'Create your <br> account',
-  description: 'Complete your account with some basic information inlcuding your home your work location and your working hours'
+  description: "Complete your account with some basic information inlcuding your home your work location and your working hours."
   },
   {
     image: 'images/solucao-2-teste-programador.png',
     title: 'Plan your <br> journeys',
-    description: 'Open the app, click the agenda tab'
+    description: "Open the app, click the agenda tab and enter your working hours for the coming week. Then you'll be presented with a list of our drivers."
   },
   {
     image: 'images/solucao-3-teste-programador.png',
     title: 'Meet your <br> driver',
-    description: ''
+    description: "Use our chat to get to know your met with our driver. You'll be picked up at your regular location, or agree on an alternative pick-up location"
   },
   {
     image: 'images/solucao-4-teste-programador.png',
     title: 'Enjoy your <br> journey',
-    description: ''
+    description: "Get in, sit back and enjoy your journey that takes you directly to your destination with some free cup of coffee from us."
   },
 ];
