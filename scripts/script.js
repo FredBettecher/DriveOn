c3bf35b5498d3a22b7cc5271c93d5b24e481d38d
FredBettecher <fredbettecher@gmail.com>
2023-09-04 13:22:49 -0300

    feat: create home page

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+const arr = [
+  {
+  image: 'images/solucao-1-teste-programador.png',
+  title: 'Create your <br> account',
+  description: 'Complete your account with some basic information inlcuding your home your work location and your working hours'
+  },
+  {
+    image: 'images/solucao-2-teste-programador.png',
+    title: 'Plan your <br> journeys',
+    description: 'Open the app, click the agenda tab'
+  },
+  {
+    image: 'images/solucao-3-teste-programador.png',
+    title: 'Meet your <br> driver',
+    description: ''
+  },
+  {
+    image: 'images/solucao-4-teste-programador.png',
+    title: 'Enjoy your <br> journey',
+    description: ''
+  },
+];
